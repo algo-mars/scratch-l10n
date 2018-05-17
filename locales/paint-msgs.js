25f9daa64ad2ccbae5b733102e439d75e71dbb24
Sergey Zhirkov <aralot@mail.ru>
2018-05-17 11:27:00 +0300

    Add "locales" and "dist" to repo (because we do not use NPM registry yet) and replace generated locales with hardcoded RU

---
+++ locales/paint-msgs.js
@@ -0,0 +1,123 @@
+// GENERATED FILE:
+const paintMsgs = {
+  "en": {
+    "messages": {
+      "paint.paintEditor.hue": "Цвет",
+      "paint.paintEditor.saturation": "Насыщенность",
+      "paint.paintEditor.brightness": "Яркость",
+      "gui.comingSoon.message1": "Don't worry, we're on it {emoji}",
+      "gui.comingSoon.message2": "Coming Soon...",
+      "gui.comingSoon.message3": "We're working on it {emoji}",
+      "paint.paintEditor.fill": "Заливка",
+      "paint.modeTools.brushSize": "Размер кисти",
+      "paint.modeTools.eraserSize": "Размер ластика",
+      "paint.modeTools.copy": "Копировать",
+      "paint.modeTools.paste": "Вставить",
+      "paint.modeTools.curved": "Кривая",
+      "paint.modeTools.pointed": "По точкам",
+      "paint.modeTools.flipHorizontal": "Отразить по горизонтали",
+      "paint.modeTools.flipVertical": "Отразить по вертикали",
+      "paint.paintEditor.costume": "Костюм",
+      "paint.paintEditor.group": "Группировать",
+      "paint.paintEditor.ungroup": "Разгруппировать",
+      "paint.paintEditor.undo": "Отменить",
+      "paint.paintEditor.redo": "Повторить",
+      "paint.paintEditor.forward": "Вперёд",
+      "paint.paintEditor.backward": "Назад",
+      "paint.paintEditor.front": "На передний план",
+      "paint.paintEditor.back": "На задний план",
+      "paint.paintEditor.more": "Ещё",
+      "paint.paintEditor.bitmap": "Конвертировать в растр",
+      "paint.paintEditor.stroke": "Контур"
+    }
+  },
+  "ar": {
+    "messages": {
+      "paint.paintEditor.redo": "إعادة",
+      "paint.paintEditor.fill": "مُعَبأ",
+      "paint.modeTools.eraserSize": "حجم الممحاة",
+      "paint.paintEditor.backward": "إلى الخلف",
+      "paint.paintEditor.group": "تجميع",
+      "paint.modeTools.paste": "لصق",
+      "paint.paintEditor.saturation": "تَشَبُع",
+      "paint.modeTools.brushSize": "حجم الفرشاة",
+      "paint.paintEditor.undo": "تَراجع",
+      "paint.paintEditor.forward": "إلى الأمام",
+      "paint.paintEditor.brightness": "شدة الإضاءة",
+      "paint.paintEditor.costume": "المظهر",
+      "paint.paintEditor.back": "الرُجُوع",
+      "paint.paintEditor.ungroup": "فك تجميع",
+      "paint.paintEditor.hue": "اللون",
+      "paint.modeTools.copy": "نسخ",
+      "paint.paintEditor.front": "الأمام",
+      "paint.paintEditor.stroke": "الخطوط العريضة"
+    }
+  },
+  "de": {
+    "messages": {
+      "paint.paintEditor.redo": "Wiederherstellen",
+      "paint.paintEditor.fill": "Füllen",
+      "paint.modeTools.eraserSize": "Eraser size",
+      "paint.paintEditor.backward": "Backward",
+      "paint.paintEditor.group": "Gruppieren",
+      "paint.modeTools.paste": "Paste",
+      "paint.paintEditor.saturation": "Sättigung",
+      "paint.modeTools.brushSize": "Brush size",
+      "paint.paintEditor.undo": "Rückgängig machen",
+      "paint.paintEditor.forward": "Forward",
+      "paint.paintEditor.brightness": "Helligkeit",
+      "paint.paintEditor.costume": "Kostüm",
+      "paint.paintEditor.back": "Zurück",
+      "paint.paintEditor.ungroup": "Gruppierung aufheben",
+      "paint.paintEditor.hue": "Color",
+      "paint.modeTools.copy": "Copy",
+      "paint.paintEditor.front": "Front",
+      "paint.paintEditor.stroke": "Umriss"
+    }
+  },
+  "es": {
+    "messages": {
+      "paint.paintEditor.redo": "Rehacer",
+      "paint.paintEditor.fill": "Rellenar",
+      "paint.modeTools.eraserSize": "Tamaño de la goma",
+      "paint.paintEditor.backward": "Atrás",
+      "paint.paintEditor.group": "Agrupar",
+      "paint.modeTools.paste": "Pegar",
+      "paint.paintEditor.saturation": "Saturación",
+      "paint.modeTools.brushSize": "Tamaño del pincel",
+      "paint.paintEditor.undo": "Deshacer",
+      "paint.paintEditor.forward": "Adelante",
+      "paint.paintEditor.brightness": "Brillo",
+      "paint.paintEditor.costume": "Disfraz",
+      "paint.paintEditor.back": "Detrás del todo",
+      "paint.paintEditor.ungroup": "Desagrupar",
+      "paint.paintEditor.hue": "Color",
+      "paint.modeTools.copy": "Copiar",
+      "paint.paintEditor.front": "Al frente",
+      "paint.paintEditor.stroke": "Contorno"
+    }
+  },
+  "he": {
+    "messages": {
+      "paint.paintEditor.redo": "Redo",
+      "paint.paintEditor.fill": "Fill",
+      "paint.modeTools.eraserSize": "Eraser size",
+      "paint.paintEditor.backward": "Backward",
+      "paint.paintEditor.group": "Group",
+      "paint.modeTools.paste": "Paste",
+      "paint.paintEditor.saturation": "Saturation",
+      "paint.modeTools.brushSize": "Brush size",
+      "paint.paintEditor.undo": "Undo",
+      "paint.paintEditor.forward": "Forward",
+      "paint.paintEditor.brightness": "Brightness",
+      "paint.paintEditor.costume": "Costume",
+      "paint.paintEditor.back": "Back",
+      "paint.paintEditor.ungroup": "Ungroup",
+      "paint.paintEditor.hue": "Color",
+      "paint.modeTools.copy": "Copy",
+      "paint.paintEditor.front": "Front",
+      "paint.paintEditor.stroke": "Outline"
+    }
+  }
+}
+exports.messages = paintMsgs;
